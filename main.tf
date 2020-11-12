54fcf702c0daeb743edb6961a7c2f3f8eff2265f
Hervé Le Meur <herve.lemeur@sixense-group.com>
2020-11-12 17:21:43 +0100

    feat(terraform): remove the cluster entries from kubeconfig on destroy

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ module "jx" {
   jx_bot_token       = var.jx_bot_token
 }
 
+output "cluster_name" {
+  description = "Cluster name"
+  value       = module.jx.cluster_name
+}
+
+output "cluster_resource_group" {
+  description = "Cluster resource group"
+  value       = module.jx.cluster_resource_group
+}
+
 output "connect" {
   description = "Connect to cluster"
   value       = module.jx.connect
